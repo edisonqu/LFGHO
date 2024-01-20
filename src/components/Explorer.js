cfb443064d5df0dcad8bd0d5538516ceaf1f1cc9
edisonqu <edisonquu@gmail.com>
2024-01-20 02:59:56 -0500

    chagne explorer to a helper function for easier access

--- src/components/Explorer.js
+++ src/components/Explorer.js
@@ -1,33 +1,22 @@
-import {React, useState} from 'react'
-import Web3 from 'web3'
-import ERC20_ABI from "../abis/erc20_abi.json"
-import { BigNumber} from "@ethersproject/bignumber";
+import Web3 from 'web3';
+import ERC20_ABI from "../abis/erc20_abi.json";
+import { BigNumber } from "@ethersproject/bignumber";
 
-const Explorer = (props) => {
+const getBalance = async (address) => {
     const web3 = new Web3(window.ethereum);
-    console.log(window.ethereum)
-    const CONTRACT_ADDRESS = "0xc4bF5CbDaBE595361438F8c6a187bDc330539c60" // the address of GHO on Sepolia
-    const [balance, setBalance] = useState(0);
-    const getBalance = async () => {
-        const erc20Contract = new web3.eth.Contract(ERC20_ABI, CONTRACT_ADDRESS)
-        try {
-            const response = await erc20Contract.methods.balanceOf(props.address).call();
-            const tokensDecimals = BigNumber.from(response);
-            const decimals = 18; // Change depending on the ERC20 token
-            const divisor = Math.pow(10, decimals); 
-            const tokens = tokensDecimals / divisor;
-            setBalance(tokens);
-            return tokens;
-        } catch (error) {
-            console.error('Error fetching balance:', error);
-            throw error;
-        }
+    const CONTRACT_ADDRESS = "0xc4bF5CbDaBE595361438F8c6a187bDc330539c60"; // the address of GHO on Sepolia
+    const erc20Contract = new web3.eth.Contract(ERC20_ABI, CONTRACT_ADDRESS);
+
+    try {
+        const response = await erc20Contract.methods.balanceOf(address).call();
+        const tokensDecimals = BigNumber.from(response);
+        const decimals = BigNumber.from(10).pow(18); // Assuming 18 decimals for the ERC20 token
+        const tokens = tokensDecimals.div(decimals);
+        return tokens.toNumber(); // Convert back to a JavaScript number, if it's within a safe range
+    } catch (error) {
+        console.error('Error fetching balance:', error);
+        throw error;
     }
-    getBalance();
+};
 
-  return (
-    <div>The balance of {props.address} is {balance} GHO</div>
-  )
-}
-
-export default Explorer
+export default getBalance;
